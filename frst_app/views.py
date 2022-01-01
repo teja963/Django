ff53520936d889b71831fd59aa1cdcfb8f0017e6
teja963 <panasateja123@gmail.com>
2022-01-01 15:41:14 +0530

    urls and views added

--- frst_app/views.py
+++ frst_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+def home(request):
+  return HttpResponse('Home Page')
+  
+def contact(request):
+  return HttpResponse('Contact Page')
+  
+def about(request):
+  return HttpResponse('About Page')
 # Create your views here.
