86c7cfb45c29c53671c597f6b94b441e3ce19641
teja963 <panasateja123@gmail.com>
2022-01-04 23:05:53 +0530

    DataBase Models and Admin Panel added

--- frst_app/admin.py
+++ frst_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(customer)
+admin.site.register(order)
+admin.site.register(product)
--- frst_app/models.py
+++ frst_app/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+class customer(models.Model):
+    name = models.CharField(max_length = 200, null = True)
+    email = models.CharField(max_length=200, null = True)
+    phone = models.CharField(max_length=13, null = True)
+    date_created = models.DateTimeField(auto_now_add=True, null = True)
+      
+    def __str__(self):
+      return self.name
+   
+
+class product(models.Model):
+    CATEGORY = (
+        ('Indoor', 'Indoor'),
+        ('Out Door','Out Door'),
+    )
+    name = models.CharField(max_length = 200, null = True)
+    price = models.FloatField(max_length = 10)
+    category = models.CharField(max_length = 200, null = True, choices = CATEGORY)
+    description =models.CharField(max_length = 200, null = True)
+    date_created = models.DateTimeField(auto_now_add = True, null = True)
+
+
+class order(models.Model):
+    STATUS = (
+          ('Pending', 'Pending'),
+          ('Out of Delivery','Out of Delivery'),
+          ('Delivered', 'Delivered'),
+    )
+    name = models.CharField(max_length = 200, null = True)
+    price = models.FloatField(max_length = 10)
+    category = models.CharField(max_length = 200, null = True)
+    status =models.CharField(max_length = 200, null = True, choices = STATUS)  # Drop down menu
+    date_created = models.DateTimeField(auto_now_add = True, null = True)
